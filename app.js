aebbb19e33cc3e334321a56cdb74b4b7ef6875fe
shreya540 <dheshinenishreya14123@gmail.com>
2025-04-27 14:05:51 +0530

    Just now updated

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.engine("ejs", ejsMate);
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema}=require("./schema.js");
+
 main().then(() => {
     console.log("Connect successfull");
 }).catch((err) => {
@@ -21,6 +25,15 @@ main().then(() => {
 })
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+        if(error){
+            throw new ExpressError(400,error);
+        }else{
+            next(); 
+        }
 }
 
 app.get("/listings", async (req, res) => {
@@ -31,33 +44,35 @@ app.get("/listings", async (req, res) => {
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 })
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
-})
-app.post("/listings", async (req, res) => {
-    const listing = req.body.listing;
-    console.log(listing);
-    const newListing = new Listing(listing);
-    await newListing.save();
-    res.redirect("/listings");
-})
-app.get("/listings/:id/edit", async (req, res) => {
+}))
+app.post("/listings",validateListing, wrapAsync(async (req, res,next) => {
+        
+        const listing = req.body.listing;
+        console.log(listing);
+        const newListing = new Listing(listing);
+        await newListing.save();
+        res.redirect("/listings");
+    }
+))
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-})
-app.put("/listings/:id", async (req, res) => {
+}))
+app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-})
-app.delete("/listings/:id", async (req, res) => {
+}))
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
+}))
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
 //         title: "My new Villa",
@@ -73,6 +88,13 @@ app.delete("/listings/:id", async (req, res) => {
 app.get("/", (req, res) => {
     console.log("Iam a root server");
     res.send("Hi, Iam root");
+})
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page not found!"));
+})
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something went wrong!"}=err;
+    res.status(statusCode).render("error.ejs",{err});
 })
 app.listen(3000, () => {
     console.log("app is listening");
--- models/listing.js
+++ models/listing.js
@@ -10,15 +10,15 @@ const listingSchema = new Schema({
         type: String,
     },
     image: {
-        type: {
-            filename: String,
-            url: String
+        filename: {
+            type: String,
+            default: "default_image"
         },
-        default: {
-            filename: "default_image",
-            url: "https://unsplash.com/photos/woman-in-gold-and-red-dress-DrKGPW1U1_M"
-        },
-        set: (v) => v === "" ? "https://unsplash.com/photos/woman-in-gold-and-red-dress-DrKGPW1U1_M" : v,
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" : v,
+        }
     },
     price: Number,
     location: String,
