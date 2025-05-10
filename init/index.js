8432c0262fde2b081ef417858a634e5edfef6b2b
shreya540 <dheshinenishreya14123@gmail.com>
2025-05-10 18:20:23 +0530

    hi

--- init/index.js
+++ init/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
+const User = require("../models/user.js");
 const initData = require("./data.js");
 main().then(() => {
     console.log("Connect successfull");
@@ -9,10 +10,14 @@ main().then(() => {
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
+
 const initDB = async () => {
-    await Listing.deleteMany({});
-    initData.data=initData.data.map((obj)=>({...obj,owner:'681efa27c3fcadba33d8edfb'}));
+    await Listing.deleteMany({}); 
+    initData.data = initData.data.map((obj) => ({
+        ...obj,
+        owner: '681f483e2aa85c5079e57049'
+    }));
     await Listing.insertMany(initData.data);
-    console.log("Data was initialized");
+    console.log("Data initialized!");
 }
 initDB();
